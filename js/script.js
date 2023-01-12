c873087064ec534d7e2a3bbaa8b331f94091d9b1
Florian Aimé <44534382+florian83440@users.noreply.github.com>
2023-01-12 14:51:32 +0100

    rect

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
-window.addEventListener('load', (event) => {
-    console.log('La page est complètement chargée');
+var canvas, ctx, w, h;
+
+
+window.onload = function init() {
+    canvas = document.querySelector("#gameCanvas");
+
+    w = canvas.width;
+    h = canvas.height;
+
+    ctx = canvas.getContext('2d');
+
+    window.addEventListener('keypress', (event) => {
+        //113 => Q, 100 => D
+        drawSpaceship(10, 10)
 
     let ctx = document.querySelector('#gameCanvas').getContext('2d');
 
     ctx.fillStyle = "#FF0000";
     ctx.fillRect(20, 20, 150, 100);
 
-});
+  });
+
+};
+
+function drawSpaceship(x, y) {
+    
+    ctx.save();
+
+    ctx.strokeRect(375, 700, 50, 70);
+    ctx.translate(x, y);
+
+    context.restore();
+}
