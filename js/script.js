049d18dc5e04ac4c7cad5cabf5c8c8868de9270b
Florian Aimé <44534382+florian83440@users.noreply.github.com>
2023-01-12 14:51:32 +0100

    rect

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 window.addEventListener('load', (event) => {
     console.log('La page est complètement chargée');
-  });
+
+    let ctx = document.querySelector('#gameCanvas').getContext('2d');
+
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(20, 20, 150, 100);
+
+});
